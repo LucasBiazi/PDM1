1bfc0792fbd7957d50d77b79c7e5ad78150fa08c
LucasBiazi <54125160+LucasBiazi@users.noreply.github.com>
2020-04-25 22:17:55 -0400

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'drona.db.elephantsql.com',
+    username: 'nblpuadi',
+    password: '9Y_K99G8Q-P55t4QjgMNVhByr7suljYL',
+    database: 'nblpuadi',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
